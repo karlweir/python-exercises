f210c60ce27c69b467eefbd6d93d15a8c8d68424
Karl Weir <karllewisweir@gmail.com>
2015-06-03 13:05:39 +0100

    Create bannerMaker.py

---
+++ python3/bannerMaker.py
@@ -0,0 +1,6 @@
+# User inputs their name and bannerMaker.py generates a banner around their name
+name = input("Enter your name: ")
+
+print('---' + '-' * len(name) + '---')
+print('.::' + name + '::.')
+print('---' + '-' * len(name) + '---')
