7a51e863495dad7ca5375079c2b776ae080e48ca
Karl Weir <karllewisweir@gmail.com>
2015-06-05 13:07:40 +0100

    Update circleCalculater.py

--- python3/circleCalculater.py
+++ python3/circleCalculater.py
@@ -1,1 +1,13 @@
+#! /usr/bin/python3
 
+# This program calulates the area, circumference and diameter of a circle based on it's radius
+
+radius = float(input('Enter the radius of your circle (cm) to work out its diameter, circumference and area: '))
+
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius * radius
+diameter = radius * 2
+
+print('Diameter: ' + str(diameter) + 'cm')
+print('Circumference: ' + str(circumference) + 'cm')
+print('Area: ' + str(area) + 'cm2')
